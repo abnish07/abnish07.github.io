24cd5bd3e5b54e971b04f235f158b5cef7e35873
Abnish <akp12556@gmail.com>
2020-05-09 12:47:57 +0530

    portfolio

---
+++ portfolio/script.js
@@ -0,0 +1,6 @@
+// GitHubCalendar(".calendar", "abnish07");
+
+    // or enable responsive functionality:
+    GitHubCalendar(".calendar", "Ssrikanth4c", { responsive: true, global_stats: true });
+
+    // const CalendarGraph = require("github-calendar-graph");
