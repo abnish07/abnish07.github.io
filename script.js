340301086dda7ae0f8c55b42541eca266d2a6334
Abnish Kumar <59315687+abnish07@users.noreply.github.com>
2020-05-09 13:22:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // GitHubCalendar(".calendar", "abnish07");
 
     // or enable responsive functionality:
-    GitHubCalendar(".calendar", "Ssrikanth4c", { responsive: true, global_stats: true });
+    GitHubCalendar(".calendar", "abnish07", { responsive: true, global_stats: true });
 
     // const CalendarGraph = require("github-calendar-graph");
